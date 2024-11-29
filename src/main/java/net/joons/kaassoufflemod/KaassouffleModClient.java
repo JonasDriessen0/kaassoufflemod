24e7dde26280471f113054b72af7379ab7064ed2
JonasDriessen0JonasDriessen0 <30167@ma-web.nl>
2024-11-29 15:28:00 +0100

    Initial commit

---
+++ src/main/java/net/joons/kaassoufflemod/KaassouffleModClient.java
@@ -0,0 +1,10 @@
+package net.joons.kaassoufflemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KaassouffleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
